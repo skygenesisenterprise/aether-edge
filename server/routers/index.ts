679fb495cda1211fd7f09549584333a3d96391b9
liam dispa <liam.dispa@skygenesisenterprise.com>
2025-11-10 19:07:55 +0100

    Update Docker & server module

---
+++ server/routers/index.ts
@@ -0,0 +1,15 @@
+// Re-export all router modules
+export * from './apiKeys';
+export * from './auditLogs';
+export * from './blueprints';
+export * from './certificates';
+export * from './domain';
+export * from './generatedLicense';
+export * from './gerbil';
+export * from './license';
+export * from './newt';
+export * from './olm';
+export * from './role';
+export * from './site';
+export * from './user';
+export * from './ws';
